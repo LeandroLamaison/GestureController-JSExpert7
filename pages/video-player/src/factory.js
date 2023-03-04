870104a0b01a826cd089cc1c465ba0a512746fc0
Leandro Lamaison <lamaisonleandro2@gmail.com>
2023-03-04 19:07:51 -0300

    ✨ implement camera service

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -1,9 +1,11 @@
 import Controller from "./controller.js"
 import View from "./view.js"
 import Service from "./service.js"
+import Camera from "../../../lib/camera.js"
 
 const factory = {
-  initialize() {
+  async initialize() {
+    await Camera.init()
     return Controller.initialize({
       view: new View(),
       service: new Service({
