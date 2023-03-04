5f36ae1df3823f1ff84a678898067d61e9e74264
Leandro Lamaison <lamaisonleandro2@gmail.com>
2023-03-04 19:42:00 -0300

    ✨ implement worker type browser support

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -2,6 +2,19 @@ import Controller from "./controller.js"
 import View from "./view.js"
 import Service from "./service.js"
 import Camera from "../../../lib/camera.js"
+import { doesSupportWorkerType } from "../../../lib/util.js"
+
+function getWorker(src) {
+  if (doesSupportWorkerType()) {
+    return new Worker(src, { type: 'module' })
+  }
+
+  const workerMock = {
+    onmessage: () => { },
+    postMessage: () => { }
+  }
+  return workerMock
+}
 
 const factory = {
   async initialize() {
@@ -9,7 +22,7 @@ const factory = {
     return Controller.initialize({
       view: new View(),
       service: new Service({
-        worker: new Worker('./src/worker.js', { type: 'module' })
+        worker: getWorker('./src/worker.js')
       })
     })
   }
