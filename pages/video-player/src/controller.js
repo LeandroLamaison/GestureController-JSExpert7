a476d151f9bd8964b20295016c4414aef4f00303
Leandro Lamaison <lamaisonleandro2@gmail.com>
2023-03-12 18:05:56 -0300

    ✨ add blink recognition button

--- pages/video-player/src/controller.js
+++ pages/video-player/src/controller.js
@@ -5,15 +5,26 @@ export default class Controller {
   constructor({ view, service }) {
     this.#view = view
     this.#service = service
+    this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
   }
 
   static initialize({ view, service }) {
-    return new Controller({ view, service }).init()
+    const controller = new Controller({ view, service })
+    controller.log('Not yet detecting eye blink! Click in the button to start.')
+    return controller.init()
   }
 
   async init() {
     console.log('Hello from controller')
     this.#view.init()
     this.#service.init()
+  }
+
+  log(text) {
+    this.#view.log(text)
+  }
+
+  onBtnStart() {
+    this.log('Initializing detection...')
   }
 }
--- pages/video-player/src/view.js
+++ pages/video-player/src/view.js
@@ -1,5 +1,20 @@
 export default class View {
+  #btnInit = document.querySelector('#init')
+  #statusElement = document.querySelector('#status')
+
   async init() {
     console.log('Hello from view')
+  }
+
+  enableButton() {
+    this.#btnInit.disabled = false
+  }
+
+  configureOnBtnClick(fn) {
+    this.#btnInit.addEventListener('click', fn)
+  }
+
+  log(text) {
+    this.#statusElement.innerHTML = text
   }
 }
